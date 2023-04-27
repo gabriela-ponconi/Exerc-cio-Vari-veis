bf2044ea807b2e35dbc47a94ed9f5f79fa60f536
gabriela-ponconi <gabiponconii@gmail.com>
2023-04-27 08:53:11 -0300

    Exercicio-variaveis

---
+++ index.js
@@ -0,0 +1,67 @@
+// console.log("hello")
+// //Comentar uma linha
+// //prompt("Digite seu nome?")
+
+// const nome = prompt("Meu nome é")
+// console.log(nome)
+
+// const idade = prompt("e eu tenho?")
+// console.log(idade)
+
+// const endereco = prompt("Digite seu endereco?")
+// console.log(endereco)
+
+// console.log ("Olá", "Meu nome é", nome, "e eu tenho", idade, "anos")
+
+// const primeiroNome = prompt ("Digite seu Primeiro Nome")
+
+// const sobrenome = prompt ("Digite seu Sobrenome")
+
+// const idade= prompt ("Digite sua Idade")
+
+// const estudante= prompt ("Sou estudante/nao sou estudante")
+
+// console.log ("Olá", "Meu nome é", primeiroNome, sobrenome, "e tenho", idade, "anos", "e",estudante)
+
+
+// const got = "Game of Thrones"
+// const temporadas = 8
+
+// console.log (typeof got)
+// console.log (typeof temporadas)
+
+// // typeof got //string
+// // typeof temporadasDeGot //number
+
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// let horas = prompt ("Quantas horas você trabalha por dia?")
+
+// let valor = prompt ("Quanto você recebe por dia?")
+
+// console.log ("Trabalho", horas, "horas por dia e recebo", valor, "reais")
+
+
+// let nome
+// let idade
+
+// console.log (typeof nome)
+// console.log (typeof idade)
+
+// nome= String (prompt ("Digite seu nome"))
+// idade= Number (prompt ("Digite sua idade"))
+
+// console.log (typeof nome)
+// console.log (typeof idade)
+
+// console.log ("Olá",nome, "você tem",idade,"anos")
+
+
+// const chuva = prompt ("Está chovendo hoje?Sim/Não")
+// const dia= prompt ("Hoje é segunda-feira?Sim/Não")
+// const curso= prompt ("Você está gostando do curso? Sim/Não")
+
+// console.log ("Está chovendo hoje",chuva,"Hoje é segunda-feira",dia, "Você está gostando do curso", curso)
